13e57405d4c5f421e9c07524ef90eb93c27e7db2
quarter-mile <131607705+quarter-mile@users.noreply.github.com>
2023-04-23 18:47:28 +0200

    Add files via upload

---
+++ day002-time_left.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+int_age = int(age)
+left = 90 - int_age
+
+days = left * 365
+weeks = left * 52
+months = left * 12
+
+print(f"You have {days} days, {weeks} weeks, or {months} months left.")
+#Write your code below this line 👇
+
+
+
+
