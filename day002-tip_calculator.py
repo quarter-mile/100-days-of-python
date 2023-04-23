9a6a7b40f580cc45dc1ea5840f419daa9e5364df
quarter-mile <131607705+quarter-mile@users.noreply.github.com>
2023-04-23 19:40:41 +0200

    Update day002-tip_calculator.py

--- day002-tip_calculator.py
+++ day002-tip_calculator.py
@@ -1,11 +1,8 @@
-bill = input("Welcome to the tip calculator.\nWhat was the total bill? $")
-bill = float(bill)
+bill = float(input("Welcome to the tip calculator.\nWhat was the total bill? $"))
 
-tip = input("What percentage of tip would you like to give? 10, 12, or 15? ")
-tip = 1 + (int(tip) / 100)
+tip = 1 + (int(input("What percentage of tip would you like to give? 10, 12, or 15? ")) / 100)
 
-people = input("How many people to split the bill? ")
-people = int(people)
+people = int(input("How many people to split the bill? "))
 
 part = round(bill / people * tip, 2)
 
