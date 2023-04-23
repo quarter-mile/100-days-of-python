a9224cf198bbbfc30a3a5ddd845675ce23ed0cef
quarter-mile <131607705+quarter-mile@users.noreply.github.com>
2023-04-23 18:49:44 +0200

    Update day001-band_name_generator.py

--- day001-band_name_generator.py
+++ day001-band_name_generator.py
@@ -1,15 +1,6 @@
-#1. Create a greeting for your program.
 print("Hello, and welcome to the Band Name Generator!")
 
-
-#2. Ask the user for the city that they grew up in.
 city = input("Which city did you grow up in?\n")
-#3. Ask the user for the name of a pet.
 pet_name = input("What was the name of your first childhood pet?\n")
 
-#4. Combine the name of their city and pet and show them their band name.
 print("The name your new band could be " + city + " " + pet_name + "!")
-
-#5. Make sure the input cursor shows on a new line:
-
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
