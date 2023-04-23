1e0c8cd7efb3ad8e48e3b7826bf9a5c290b4cb50
quarter-mile <131607705+quarter-mile@users.noreply.github.com>
2023-04-23 18:35:11 +0200

    Add files via upload

---
+++ day002-bmi_calculator.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+# 🚨 Don't change the code above 👆
+fl_h = float(height)
+fl_w = float(weight)
+
+bmi =  fl_w/ fl_h ** 2
+
+print(int(bmi))
+#Write your code below this line 👇
